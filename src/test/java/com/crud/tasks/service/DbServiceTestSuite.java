596ee95cf5dc374ee71f7be497fbaaae132c88f8
Mateusz Dziadowicz <dziadulus@hotmail.com>
2021-06-29 18:21:28 +0200

    29.2 Coverage DvService tests

--- src/test/java/com/crud/tasks/service/DbServiceTestSuite.java
+++ src/test/java/com/crud/tasks/service/DbServiceTestSuite.java
@@ -8,8 +8,12 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class DbServiceTestSuite {
@@ -21,21 +25,61 @@ public class DbServiceTestSuite {
     TaskRepository repository;
 
     @Test
-    public void dbServiceTest() {
+    public void dbServiceSaveTaskTest() {
 
         //Given
         Task task  = new Task(0L, "test", "test");
+        when(repository.save(task)).thenReturn(task);
 
         //When
-        dbService.saveTask(task);
-        dbService.getTaskById(0L);
-        dbService.getAllTasks();
-        dbService.deleteTask(0L);
+        Task savedTask = dbService.saveTask(task);
 
         //Then
         verify(repository, times(1)).save(task);
-        verify(repository, times(1)).findById(0L);
+        assertEquals(task, savedTask);
+    }
+
+    @Test
+    public void dbServiceGetAllTasksTest() {
+
+        //Given
+        Task task  = new Task(0L, "test", "test");
+        List<Task> tasks = new ArrayList<>();
+        tasks.add(task);
+        when(repository.findAll()).thenReturn(tasks);
+
+        //When
+        List<Task> resultTasks = dbService.getAllTasks();
+
+        //Then
         verify(repository, times(1)).findAll();
+        assertEquals(tasks, resultTasks);
+    }
+
+    @Test
+    public void dbServiceFindByIdTest() {
+
+        //Given
+        Task task  = new Task(0L, "test", "test");
+        when(repository.findById(0L)).thenReturn(Optional.of(task));
+
+        //When
+        Task savedTask = dbService.getTaskById(0L).get();
+
+        //Then
+        verify(repository, times(1)).findById(0L);
+        assertEquals(task, savedTask);
+    }
+
+    @Test
+    public void dbServiceDeleteTaskTest() {
+
+        //Given
+
+        //When
+        dbService.deleteTask(0L);
+
+        //Then
         verify(repository, times(1)).deleteById(0L);
     }
 }
